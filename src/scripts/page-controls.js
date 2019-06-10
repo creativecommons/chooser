7c316df9cc499b8e5e4c44c5ffabc16fca1da052
Ari <akmadian@gmail.com>
2019-06-10 13:53:50 -0700

    Fix font, add basic chooser

--- src/scripts/page-controls.js
+++ src/scripts/page-controls.js
@@ -1,7 +1,7 @@
 function spawn_modal(modal_id) {
-    app_state.modals.modal_content = app_state.modals.modal_packs[modal_id].content
-    app_state.modals.modal_title = app_state.modals.modal_packs[modal_id].title
-    app_state.modals.modal_toggle = "modal is-active"
+    app_state.modals.modal_content = app_state.modals.modal_packs[modal_id].content // Update modal content
+    app_state.modals.modal_title = app_state.modals.modal_packs[modal_id].title // Update modal title
+    app_state.modals.modal_toggle = "modal is-active" // Show modal
 }
 
 function destroy_modal() {
