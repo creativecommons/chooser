bb68092f6cf12848487352a81121820e30c28692
Ari <akmadian@gmail.com>
2019-07-25 19:14:27 -0700

    Refactor to Vue base

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Buefy from 'buefy'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+Vue.use(Buefy)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
