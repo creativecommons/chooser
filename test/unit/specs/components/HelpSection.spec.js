9b9552b12d377bd37f3c53e1335cb2c80a67813d
Matheus Joranhezon <mjoranhezon@gmail.com>
2019-10-03 22:39:59 -0300

    Update unit tests to click on buttons by their id's.

--- test/unit/specs/components/HelpSection.spec.js
+++ test/unit/specs/components/HelpSection.spec.js
@@ -26,59 +26,59 @@ describe('HelpSection.vue', () => {
     });
 
     it("expects isModal1Active to be true after clicking it", () => {
-        const button = wrapper.find('button');
+        const button = wrapper.find('#cc_license_btn');
         button.trigger('click');
         expect(wrapper.vm.isModal1Active).toEqual(true);
     });
 
     it("equals isModal2Active to false", () => {
-        expect(wrapper.vm.isModal1Active).toEqual(false);
+        expect(wrapper.vm.isModal2Active).toEqual(false);
     });
 
     it("expects isModal2Active to be true after clicking it", () => {
-        const button = wrapper.find('button');
+        const button = wrapper.find('#license_work_btn');
         button.trigger('click');
-        expect(wrapper.vm.isModal1Active).toEqual(true);
+        expect(wrapper.vm.isModal2Active).toEqual(true);
     });
 
     it("equals isModal3Active to false", () => {
-        expect(wrapper.vm.isModal1Active).toEqual(false);
+        expect(wrapper.vm.isModal3Active).toEqual(false);
     });
 
     it("expects isModal3Active to be true after clicking it", () => {
-        const button = wrapper.find('button');
+        const button = wrapper.find('#public_domain_btn');
         button.trigger('click');
-        expect(wrapper.vm.isModal1Active).toEqual(true);
+        expect(wrapper.vm.isModal3Active).toEqual(true);
     });
 
     it("equals isModal4Active to false", () => {
-        expect(wrapper.vm.isModal1Active).toEqual(false);
+        expect(wrapper.vm.isModal4Active).toEqual(false);
     });
 
     it("expects isModal4Active to be true after clicking it", () => {
-        const button = wrapper.find('button');
+        const button = wrapper.find('#consideration_btn');
         button.trigger('click');
-        expect(wrapper.vm.isModal1Active).toEqual(true);
+        expect(wrapper.vm.isModal4Active).toEqual(true);
     });
 
     it("equals isModal5Active to false", () => {
-        expect(wrapper.vm.isModal1Active).toEqual(false);
+        expect(wrapper.vm.isModal5Active).toEqual(false);
     });
 
     it("expects isModal5Active to be true after clicking it", () => {
-        const button = wrapper.find('button');
+        const button = wrapper.find('#formal_license_btn');
         button.trigger('click');
-        expect(wrapper.vm.isModal1Active).toEqual(true);
+        expect(wrapper.vm.isModal5Active).toEqual(true);
     });
 
     it("equals isModal6Active to false", () => {
-        expect(wrapper.vm.isModal1Active).toEqual(false);
+        expect(wrapper.vm.isModal6Active).toEqual(false);
     });
 
     it("expects isModal6Active to be true after clicking it", () => {
-        const button = wrapper.find('button');
+        const button = wrapper.find('#license_description_btn');
         button.trigger('click');
-        expect(wrapper.vm.isModal1Active).toEqual(true);
+        expect(wrapper.vm.isModal6Active).toEqual(true);
     });
 
 })
