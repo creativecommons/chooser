e9c4aa1922ff9c146630554f0b65fac8dea083a9
7_5P <76pollet@gmail.com>
2019-10-07 16:19:10 +0300

    rename function

--- test/unit/specs/components/LicenseDescription.spec.js
+++ test/unit/specs/components/LicenseDescription.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import LicenseDescription from '@/components/LicenseDescription'
 
 describe('LicenseDescription.vue', () => {
-  test('the initial text starts with the name of the selected license', () => {
+  test('the initial text starts with the name of the selected license without the version', () => {
     const desc = 'As the most accommodating of                     licenses offered, the CC BY license allows others                     to distribute, remix, tweak, and build upon your                     work, even commercially, as long as they credit                     you for the original creation.'
     const wrapper = mount(LicenseDescription, {
       propsData: {
