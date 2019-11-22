58327cd180701db64e6ef710711b46bfc532930d
Samuel Onyijne <samuelord1@gmail.com>
2019-11-22 02:32:09 +0100

    added e2e test for SelectedLicense component.

---
+++ test/e2e/specs/components/SelectedLicense.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'SelectedLicense.vue - testing the #SelectedLicense contents': function(browser) {
+        browser
+            .url(browser.globals.devServerURL)
+            .waitForElementVisible('#app', 5000)
+            .assert.elementPresent('#SelectedLicense')
+            .assert.elementPresent('.title is-2')
+            .assert.containsText('.title is-2', 'Selected License')
+            .expect.element('.license-icons').to.be.present.after(1000)
+    }
+}
