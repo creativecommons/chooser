e43d654e5d176845efb15b2abe20b36032ef02a3
Olga Bulat <obulat@gmail.com>
2019-12-25 21:13:35 +0300

    Created LicenseCard components Parent of SelectedLicenseCode.vue, SelectedLicenseDropdown.vue and SelectedLicenseInfo.vue
    Signed-off-by: Olga Bulat <obulat@gmail.com>

---
+++ src/components/LicenseCard.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class='column'>
+        <div class="card chooser-selected">
+            <header class="card-header">
+                <h2
+                    :class="'vocab-h2'">Your License</h2>
+            </header>
+            <div class="card-content">
+                <SelectedLicenseDropdown
+                    v-model="value.shortName"
+                />
+                <SelectedLicenseInfo
+                    v-model="value"
+                />
+                <SelectedLicenseHTMLGenerator
+                    v-model="value"
+                />
+            </div>
+        </div>
+    </div>
+
+</template>
+<script>
+
+import SelectedLicenseInfo from './SelectedLicenseInfo'
+import SelectedLicenseDropdown from './SelectedLicenseDropdown'
+import SelectedLicenseHTMLGenerator from './SelectedLicenseCode'
+
+export default {
+    name: 'SelectedLicenseCard',
+    props: ['value'],
+    components: {
+        SelectedLicenseHTMLGenerator,
+        SelectedLicenseDropdown,
+        SelectedLicenseInfo
+    },
+    methods: {
+        updateLicense() {
+            this.$emit('input', {
+                shortName: this.shortLicenseName,
+                fullName: this.fullLicenseName
+            })
+        }
+    },
+    watch: {
+        value() {
+            this.$emit('input', this.value)
+        }
+    }
+}
+</script>
+<style scoped>
+    header.card-header {
+        justify-content: center;
+        padding-top: 1rem;
+    }
+    h2.vocab-h2{
+        font-family: Source Sans Pro;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 25px;
+        line-height: 38px;
+        color: #333333;
+    }
+    .card-content {
+        padding-top: 0.5rem;
+    }
+    h3 {
+        font-weight: 700;
+        font-size:1.5874rem;
+    }
+    .selected-license-names b {
+        font-size: 1.8rem;
+    }
+    b {
+        text-align: center;
+    }
+</style>
