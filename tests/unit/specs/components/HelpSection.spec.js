f1a9564e908e56d283a1e30f646a6922779084b3
Olga Bulat <obulat@gmail.com>
2020-01-21 18:49:31 +0300

    Update Help Section tests
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- tests/unit/specs/components/HelpSection.spec.js
+++ tests/unit/specs/components/HelpSection.spec.js
@@ -1,7 +1,9 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import {shallowMount, createLocalVue, config} from '@vue/test-utils'
 import HelpSection from '@/components/HelpSection'
 import Buefy from 'buefy'
 import LicenseUtilities from '@/utils/license-utilities'
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 
 describe('HelpSection.vue', () => {
     let wrapper
@@ -11,7 +13,17 @@ describe('HelpSection.vue', () => {
         const localVue = createLocalVue()
         localVue.use(Buefy)
         localVue.use(LicenseUtilities)
-
+        Vue.use(VueI18n)
+        const messages = require('@/locales/en.json')
+        const i18n = new VueI18n({
+            locale: 'en',
+            fallbackLocale: 'en',
+            messages: messages
+        })
+        config.mocks.i18n = i18n
+        config.mocks.$t = (key) => {
+            return i18n.messages[key]
+        }
         wrapper = shallowMount(HelpSection, {
             localVue
         })
@@ -48,7 +60,7 @@ describe('HelpSection.vue', () => {
     })
 
     it('expects isModal3Active to be true after clicking it', () => {
-        const button = wrapper.find('#public_domain_btn')
+        const button = wrapper.find('#icons_meaning_btn')
         button.trigger('click')
         expect(wrapper.vm.isModal3Active).toEqual(true)
     })
--- tests/unit/specs/components/LicenseDropdown.spec.js
+++ tests/unit/specs/components/LicenseDropdown.spec.js
@@ -1,9 +1,9 @@
-import {config, createLocalVue, mount} from '@vue/test-utils'
-import SelectedLicenseDropdown from '@/components/SelectedLicenseDropdown'
-import Buefy from 'buefy'
-import LicenseUtilities from '@/utils/license-utilities'
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import Buefy from 'buefy'
+import {config, createLocalVue, mount} from '@vue/test-utils'
+import LicenseUtilities from '@/utils/license-utilities'
+import SelectedLicenseDropdown from '@/components/SelectedLicenseDropdown'
 
 const value = {
     shortName: 'CC BY 4.0',
@@ -29,9 +29,7 @@ describe('LicenseDropdown.vue', () => {
             fallbackLocale: 'en',
             messages: messages
         })
-
         config.mocks.i18n = i18n
-
         config.mocks.$t = (key) => {
             return i18n.messages[key]
         }
@@ -43,14 +41,12 @@ describe('LicenseDropdown.vue', () => {
         })
     })
     describe('correctly displays initial information and options when mounted', () => {
-
         it('initializes with base license names', () => {
             expect(wrapper.vm.$props.value.shortName).toEqual('CC BY 4.0')
             expect(wrapper.vm.$props.value.fullName).toEqual('Attribution 4.0 International')
             expect(wrapper.html()).toContain('CC BY 4.0')
             expect(wrapper.findAll('option')).toHaveLength(7)
         })
-
         it('calculate fullLicenseName correctly', () => {
             wrapper.vm.value.shortName = 'CC BY-ND 4.0'
             expect(wrapper.vm.fullLicenseName('CC BY-ND 4.0')).toEqual('Attribution-NoDerivatives 4.0 International')
