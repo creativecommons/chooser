8b146d6899b53a2543c88d5f5387ad36978d9c93
Olga Bulat <obulat@gmail.com>
2020-02-10 23:35:18 +0300

    Add Dropdown step
    Signed-off-by: Olga Bulat <obulat@gmail.com>

---
+++ src/components/DropdownStep.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card-content step-card-content">
+        <div class="step-description" v-if="this.status==='previous'">
+            {{cardText}}
+        </div>
+        <div class="step-actions" v-else-if="this.status==='current'">
+            <SelectedLicenseDropdown />
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SelectedLicenseDropdown from './SelectedLicenseDropdown'
+export default {
+    name: 'DropdownStep',
+    components: { SelectedLicenseDropdown },
+    props: {
+        status: String
+    },
+    computed: {
+        ...mapGetters(['fullName']),
+        cardText() {
+            return this.fullName
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
