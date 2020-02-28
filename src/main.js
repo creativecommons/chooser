7b23870d71434c5d88ab299729007be9880943bf
Ari <akmadian@gmail.com>
2020-02-28 12:02:07 -0800

    Move vocab.scss to src/styles

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import i18n from './i18n'
 import VueAnalytics from 'vue-analytics'
 import App from './App.vue'
 import store from './store'
-import './assets/css/vocab.css'
+import './styles/vocab.scss'
 
 import * as Sentry from '@sentry/browser'
 import * as Integrations from '@sentry/integrations'
