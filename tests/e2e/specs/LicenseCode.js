fdadfe67aeea90169bd90b0e0a14ce829ddaebda
hemanth-hk <uppadahemanthkumar27@gmail.com>
2020-03-10 14:59:46 +0530

    Modified and added some e2e tests for LicenseCode.vue

--- tests/e2e/specs/LicenseCode.js
+++ tests/e2e/specs/LicenseCode.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    'Check if the main p tag with the class license-text is present': function(browser) {
+    'Check if the p-tag with the class license-text is present': function(browser) {
         browser
             .init()
             .assert.elementPresent('.control-label > span')
@@ -14,11 +14,28 @@ module.exports = {
             .click('.pagination-next')
             .assert.elementPresent('p[class="license-text"]')
     },
-    'Check if the a tag in the "Rich Text" redirects to https://creativecommons.org/licenses/by-sa/4.0/?ref=ccchooser': function(browser) {
+    'Check if the links in the license code redirects properly': function(browser) {
         browser
+            .setValue('input[placeholder="Jane Doe"]', 'Jane Doe')
+            .setValue('input[placeholder="www.author.com"]', 'www.author.com')
+            .setValue('input[placeholder="This work"]', 'This work')
+            .setValue('input[placeholder="www.author.com/work.jpg"]', 'www.author.com/work.jpg')
             .assert.elementPresent('p[class="license-text"] a')
-            .getAttribute('p[class="license-text"] a', 'href', function(result) {
+            .getAttribute('p > span > a:nth-child(1)', 'href', function(result) {
+                this.assert.equal(result.value, 'http://localhost:8080/www.author.com/work.jpg')
+            })
+            .getAttribute('p > span > a:nth-child(2)', 'href', function(result) {
+                this.assert.equal(result.value, 'http://localhost:8080/www.author.com')
+            })
+            .getAttribute('p > span > a:nth-child(4)', 'href', function(result) {
                 this.assert.equal(result.value, 'https://creativecommons.org/licenses/by-sa/4.0/?ref=ccchooser')
             })
+    },
+    'Check if the text is displayed under the print attribution': function(browser) {
+        browser
+            .click('nav > ul > li:nth-child(2) > a')
+            .assert.elementPresent('p[class="license-text"]')
+            .assert.elementPresent('#attribution-text > p > span:nth-child(1)')
+            .assert.elementPresent('#attribution-text > p > span:nth-child(2)')
     }
 }
