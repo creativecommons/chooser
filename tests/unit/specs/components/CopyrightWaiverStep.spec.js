43f6003e433c62c3c23f15f1f56a04b52e984711
JackieBinya <jacquelinebinya@gmail.com>
2020-04-02 22:36:55 +0200

    Adds more unit tests

--- tests/unit/specs/components/CopyrightWaiverStep.spec.js
+++ tests/unit/specs/components/CopyrightWaiverStep.spec.js
@@ -90,6 +90,7 @@ describe('Test functionality of computed properties', () => {
         checkbox.setChecked()
 
         expect(wrapper.emitted().change[0]).toStrictEqual(['CW', 6, true])
+        expect(wrapper.vm.copyrightWaiverAgreed).toBe(true)
     })
 
     it('User checks confirmed with agreed checked', () => {
@@ -102,5 +103,55 @@ describe('Test functionality of computed properties', () => {
         checkbox.setChecked()
 
         expect(wrapper.emitted().change[0]).toStrictEqual(['CW', 6, true])
+        expect(wrapper.vm.copyrightWaiverConfirmed).toBe(true)
+    })
+})
+
+describe('Computed properties: user unchecks agreed and/ confirmed', () => {
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = mount(CopyrightWaiverStep, {
+            localVue,
+            propsData: {
+                selected: true,
+                status: 'current',
+                stepId: 6,
+                stepName: 'CW'
+            },
+            mocks: {
+                $t: key => key
+            }
+        })
+    })
+
+    afterEach(() => {
+        wrapper.destroy()
+    })
+
+    it('User unchecks agreed', () => {
+        wrapper.setData({
+            agreed: true,
+            confirmed: true
+        })
+
+        const checkbox = wrapper.findAll('input[type="checkbox"]').at(0)
+        checkbox.trigger('change')
+
+        expect(wrapper.emitted().change[0]).toStrictEqual(['CW', 6, undefined])
+        expect(wrapper.vm.copyrightWaiverAgreed).toBe(false)
+    })
+
+    it('User unchecks confirmed', () => {
+        wrapper.setData({
+            agreed: true,
+            confirmed: true
+        })
+
+        const checkbox = wrapper.findAll('input[type="checkbox"]').at(1)
+        checkbox.trigger('change')
+
+        expect(wrapper.emitted().change[0]).toStrictEqual(['CW', 6, undefined])
+        expect(wrapper.vm.copyrightWaiverConfirmed).toBe(false)
     })
 })
