465fab53f2ffebba43e798bee803f14d64cf308a
JackieBinya <jacquelinebinya@gmail.com>
2020-04-06 21:17:16 +0200

    Modifies unit tests and deletes old test file in wrong location

--- tests/e2e/specs/FirstStep.js
+++ tests/e2e/specs/FirstStep.js
@@ -2,17 +2,26 @@ module.exports = {
     '@tags': ['first'],
     'FirstStep'(browser) {
         const radioSelector = 'div.field.not-selected > label > span.control-label > span'
-        const radioSelected = 'div.field.selected > label > span.control-label > span'
-        const nextBtn = '.pagination-next'
-        const stepTitle = 'div.step-header > h5'
+        const nextBtn = 'a.pagination-next'
         const stepBelowCurrentHeader = 'div:nth-child(2) > div > h5'
+        const stepDescription = '.step-description'
+        const backBtn = 'a.pagination-previous'
 
         browser
             .init()
+            .waitForElementVisible('body')
+            .assert.not.elementPresent(stepDescription)
             .click(radioSelector)
             .assert.containsText(stepBelowCurrentHeader, 'Creative Commons License')
+            .click(nextBtn)
+            .assert.elementPresent(stepDescription)
+            .assert.visible(stepDescription)
+            .assert.containsText(stepDescription, 'I know which license I need.')
+            .click(backBtn)
             .click(radioSelector)
             .assert.containsText(stepBelowCurrentHeader, 'Attribution')
+            .click(nextBtn)
+            .assert.containsText(stepDescription, 'I need help selecting a license.')
             .end()
     }
 }
