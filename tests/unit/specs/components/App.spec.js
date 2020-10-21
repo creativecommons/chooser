d42039fe5ab6d519eea96df898dfc7878f3deffd
Olga Bulat <obulat@gmail.com>
2020-10-21 16:52:56 +0300

    Fix linting errors

--- tests/unit/specs/components/App.spec.js
+++ tests/unit/specs/components/App.spec.js
@@ -12,32 +12,32 @@ describe('App.vue', () => {
 
     // Always creates a shallow instance of component
     beforeEach(() => {
-      localVue = createLocalVue()
-      localVue.use(Vuex)
-      localVue.use(Buefy)
-      Vue.use(VueI18n)
-      const messages = require('@/locales/en.json')
-      const i18n = new VueI18n({
-          locale: 'en',
-          fallbackLocale: 'en',
-          messages: messages
-      })
-  
-      config.mocks.i18n = i18n
-  
-      config.mocks.$t = key => {
-          // key is a string (eg. 'stepper.ND.question')
-          // this line converts it into an object reference
-          // eg. messages['stepper.ND.question'] -> messages.stepper.ND.question
-          return key.split('.').reduce((messages, k) => messages[k], i18n.messages)
-      }
-      wrapper = shallowMount(App, {
-          store,
-          localVue
-      })
-      wrapper.vm.$on('input', (newVal) => {
-          wrapper.setProps({ value: newVal })
-      })
+        localVue = createLocalVue()
+        localVue.use(Vuex)
+        localVue.use(Buefy)
+        Vue.use(VueI18n)
+        const messages = require('@/locales/en.json')
+        const i18n = new VueI18n({
+            locale: 'en',
+            fallbackLocale: 'en',
+            messages: messages
+        })
+
+        config.mocks.i18n = i18n
+
+        config.mocks.$t = key => {
+            // key is a string (eg. 'stepper.ND.question')
+            // this line converts it into an object reference
+            // eg. messages['stepper.ND.question'] -> messages.stepper.ND.question
+            return key.split('.').reduce((messages, k) => messages[k], i18n.messages)
+        }
+        wrapper = shallowMount(App, {
+            store,
+            localVue
+        })
+        wrapper.vm.$on('input', (newVal) => {
+            wrapper.setProps({ value: newVal })
+        })
     })
 
     it('Check if App.vue component renders without any errors', () => {
