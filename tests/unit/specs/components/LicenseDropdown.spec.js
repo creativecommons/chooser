dea96b6e02dddda340177038e59b1e830839f8a7
Olga Bulat <obulat@gmail.com>
2020-11-04 08:46:15 +0300

    Update test for LicenseDropdown handling of selection

--- tests/unit/specs/components/LicenseDropdown.spec.js
+++ tests/unit/specs/components/LicenseDropdown.spec.js
@@ -1,71 +1,55 @@
-import { mount, createLocalVue, config } from '@vue/test-utils'
+import { createLocalVue, mount } from '@vue/test-utils'
 import LicenseDropdown from '@/components/LicenseDropdown.vue'
 import VueI18n from 'vue-i18n'
 import Buefy from 'buefy'
-import Vuex from 'vuex'
 
-describe('LicenseDropdown.vue',()=>{
+describe('LicenseDropdown.vue', () => {
     let wrapper
     let getters
-    let store
-   
-    beforeEach(()=>{
+    let storeMock
+
+    beforeEach(() => {
         const localVue = createLocalVue()
         localVue.use(VueI18n)
         localVue.use(Buefy)
-        localVue.use(Vuex)
-        getters= {
+
+        getters = {
             shortName: () => {
                 return 'CC BY-SA 4.0'
             },
             fullName: () => {
                 return 'Attribution-ShareAlike 4.0 International'
+            }
+        }
+        storeMock = {
+            commit: jest.fn(),
+            getters: getters
+        }
+
+        const options = {
+            mocks: {
+                $store: storeMock,
+                $t: (key) => key
             },
+            localVue
         }
-        store = new Vuex.Store({
-            getters
-        })
-        const messages = require('@/locales/en.json')
-        const i18n = new VueI18n({
-            locale: 'en',
-            fallbackLocale: 'en',
-            messages: messages,
-            silentTranslationWarn: true
-        })
-
-        config.mocks.i18n = i18n
-
-        config.mocks.i18n.$t = (key) => {
-            return i18n.messages[key]
-        }
-        wrapper = mount(LicenseDropdown, {
-            localVue,
-            store,
-            i18n
-        })
+        wrapper = mount(LicenseDropdown, options)
     })
 
     it('Check if LicenseDropdown.vue component renders without any errors', () => {
         expect(wrapper.isVueInstance()).toBe(true)
     })
-    it('Check if b-field tag with class license-dropdown is present in the DOM',()=>{
-        expect(wrapper.contains('.license-dropdown')).toBe(true)
-    })
-    it('Check if select tag is present in the DOM',()=>{
-        expect(wrapper.contains('select')).toBe(true)
-    })
-    it('Check if option tag is present in the DOM',()=>{
-        expect(wrapper.contains('option')).toBe(true)
-    })
 
-    it('Checks methods: setCurrentLicense',()=>{
+    it('Checks methods: setCurrentLicense', () => {
         const options = wrapper.find('select').findAll('option')
         options.at(1).setSelected()
+        const SELECTED_SHORT = options.at(1).text()
+        expect(storeMock.commit).toHaveBeenCalledWith('updateAttributesFromShort', SELECTED_SHORT)
+
         expect(wrapper.emitted().input).toBeTruthy()
     })
 
     it('Check if the LicenseDropdown.vue component has the expected UI', () => {
         expect(wrapper).toMatchSnapshot()
     })
-
 })
