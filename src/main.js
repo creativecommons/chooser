b5de735f3d65ea568b3701a1f8a45b012174f239
Olga Bulat <obulat@gmail.com>
2021-01-06 10:23:09 +0300

    Make the bundle size smaller by updating the sentry integration (#280)
    * Add Vocabulary Select implementation instead of buefy select
    
    * Revert "Add Vocabulary Select implementation instead of buefy select"
    
    This reverts commit 6f09c179
    
    * Replace sentry packages with `@sentry/vue`

--- src/main.js
+++ src/main.js
@@ -8,8 +8,7 @@ import './styles/vocab.scss'
 import VueScrollTo from 'vue-scrollto'
 
 // Analytics
-import * as Sentry from '@sentry/browser'
-import * as Integrations from '@sentry/integrations'
+import * as Sentry from '@sentry/vue'
 import Hotjar from 'vue-hotjar'
 import VueAnalytics from 'vue-analytics'
 
@@ -31,13 +30,7 @@ Sentry.init({
     dsn: process.env.NODE_ENV === 'production'
         ? 'https://8c09726e231d4cf780c541f40d3639a9@sentry.io/3009295' // cc-chooser-prod project
         : 'https://ab63acb8c1464466869182dd53c7046d@sentry.io/3009597', // cc-chooser-dev project
-    integrations: [
-        new Integrations.Vue({
-            Vue,
-            attachProps: true,
-            logErrors: process.env.NODE_ENV !== 'production' // Only log errors in dev env
-        })
-    ]
+    logErrors: process.env.NODE_ENV !== 'production' // Only log errors in dev env
 })
 
 // Production only since we only have a prod id
