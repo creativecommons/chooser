5743f8e265ff35cdf4d8b3f86b99fb04cd52fdf7
Cronus1007 <lit2019058@iiitl.ac.in>
2021-02-22 10:34:08 +0530

    small copy changes in CC0 and CC-By-ND

--- src/components/LicenseDetailsCard.vue
+++ src/components/LicenseDetailsCard.vue
@@ -50,7 +50,7 @@
             target="_blank"
             rel="noopener noreferrer"
         >
-            See the License Deed
+            See the CC0 Deed
             <span class="icon external-link">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
