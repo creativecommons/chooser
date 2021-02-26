50d10f1b1eaccd51bde4e53960859409f80fa435
Olga Bulat <obulat@gmail.com>
2021-02-26 15:24:29 +0300

    rebase on master

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -25,55 +25,55 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 Cypress.Commands.add("clickNext", () => {
-    cy.get('button').contains('NEXT').click()
+  cy.get('button').contains('NEXT').click()
 })
 Cypress.Commands.add("clickBack", () => {
-    cy.get('button').contains('BACK').click()
+  cy.get('button').contains('BACK').click()
 })
 
 Cypress.Commands.add("makeAChoice", (stepId, choice) => {
-    cy.get(stepId).find(`[value="${choice}"]`).check()
+  cy.get(stepId).find(`[value="${choice}"]`).check()
 })
 
 Cypress.Commands.add('waiveCopyright', () => {
-    cy.get('.v-checkbox:nth-child(1) > input').check()
-    cy.get('.v-checkbox:nth-child(2) > input').check()
+  cy.get('.v-checkbox:nth-child(1) > input').check()
+  cy.get('.v-checkbox:nth-child(2) > input').check()
 })
 
 Cypress.Commands.add('hasRecommendedLicense', (license) => {
-    cy.get('.recommended-card').should('be.visible').contains(license)
+  cy.get('.recommended-card').should('be.visible').contains(license)
 })
 
 Cypress.Commands.add('hasLicenseInAttributionCode', (license) => {
-    cy.get('.license-use-card').should('be.visible').contains(license)
+  cy.get('.license-use-card').should('be.visible').contains(license)
 })
 Cypress.Commands.add('hasStepsCount', (stepsCount) => {
-    cy.get('.stepper__container').find('.step-container').should('have.lengthOf', stepsCount)
+  cy.get('.stepper__container').find('.step-container').should('have.lengthOf', stepsCount)
 })
 Cypress.Commands.add('missingLicenseInAttributionCode', () => {
-    cy.get('.license-use-card').should('not.exist')
+  cy.get('.license-use-card').should('not.exist')
 })
 Cypress.Commands.add('headerTitle', (license) => {
-    cy.get('.license-short-name > .b-header').contains(license)
+  cy.get('.license-short-name > .b-header').contains(license)
 })
 Cypress.Commands.add('readableStringTitle', (license) => {
-    cy.get('b').contains(license)
+  cy.get('b').contains(license)
 })
 Cypress.Commands.add('licenseText', (license) => {
-    cy.get('.license-text > a').contains(license)
+  cy.get('.license-text > a').contains(license)
 })
 Cypress.Commands.add('copyButton', (text) => {
-    cy.get('.donate').contains(text).click()
+  cy.get('.donate').contains(text).click()
 })
 Cypress.Commands.add('selectInputField', (id, text) => {
-    cy.get(`:nth-child(${id}) > label.is-normal > .control-inner > .input`).type(text)
+  cy.get(`:nth-child(${id}) > label.is-normal > .control-inner > .input`).type(text)
 })
 Cypress.Commands.add('valueOfInputField', (id, attr) => {
-    cy.get(`:nth-child(${id}) > label.is-normal > .control-inner > .input`).invoke('val')
-        .then((text) => {
-            cy.get(`${attr}`).contains(text)
-        })
+  cy.get(`:nth-child(${id}) > label.is-normal > .control-inner > .input`).invoke('val')
+    .then((text) => {
+      cy.get(`${attr}`).contains(text)
+    })
 })
 Cypress.Commands.add('toggleButton', () => {
-    cy.get('#copy-type').click()
+  cy.get('#copy-type').click()
 })
