93746cdf4f31c79fd826be798c190bba8612e10e
possumbilities <109087089+possumbilities@users.noreply.github.com>
2024-11-18 22:12:32 +0000

    clean up the logic paths to prep for better pathstate flow

--- src/scripts.js
+++ src/scripts.js
@@ -25,112 +25,19 @@ let last = statePath[statePath.length - 1].split("&")
 let license = last[1].split("=");
 
 
-console.log(statePath[0]);
-console.log(statePath[1]);
-
-console.log(statePath[2]);
-console.log(statePath[3]);
-
-console.log(statePath[4]);
 
 
-console.log(statePath[5]);
-console.log(statePath[6]);
-console.log(statePath[7]);
-console.log(statePath[8]);
-console.log(statePath[9]);
-console.log(statePath[10]);
-console.log(statePath[11]);
+const fieldsets = document.querySelectorAll('fieldset');
 
+fieldsets.forEach((element, index) => {
 
-console.log(last[0]);
+    element.addEventListener("change", (event) => {
+        console.log(index);
+        //console.log(element.id);
+        //console.log(event.target.value);
+       
+        console.log(element.id + '/' + event.target.value + '/' );
 
-console.log(license[1]);
-
-
-
-
-document.querySelector('#require-attribution').classList.toggle('disable');
-
-document.querySelector('#allow-commercial-use').classList.toggle('disable');
-
-document.querySelector('#allow-derivatives').classList.toggle('disable');
-
-document.querySelector('#share-alike').classList.toggle('disable');
-
-document.querySelector('#waive-your-copyright').classList.toggle('disable');
-
-document.querySelector('#confirmation').classList.toggle('disable');
-
-
-
-// Get the form
-const form = document.forms[0];
-
-// Get the form's radio buttons
-const helpChoice = form.elements["choose"];
-
-// Choose the "red" option
-console.log(helpChoice.value);
-
-const selectElement = document.querySelector("#do-you-know-which-license-you-need");
-
-selectElement.addEventListener("change", (event) => {
-    console.log(helpChoice.value);
-
-    if (helpChoice.value == 'no') {
-
-        document.querySelector('#which-license-do-you-need').classList.toggle('disable');
-
-    } else {
-
-        document.querySelector('#which-license-do-you-need').classList.remove('disable');
-    }
-
-        document.querySelector('#require-attribution').classList.toggle('disable');
-
-        document.querySelector('#allow-commercial-use').classList.toggle('disable');
-
-        document.querySelector('#allow-derivatives').classList.toggle('disable');
-
-        document.querySelector('#share-alike').classList.toggle('disable');
-
-        document.querySelector('#confirmation').classList.toggle('disable');
-});
-
-
-const licenseElement = document.querySelector("#which-license-do-you-need");
-
-const licenseChoice = form.elements["license"];
-
-licenseElement.addEventListener("change", (event) => {
-    console.log(licenseChoice.value);
-
-    if (licenseChoice.value == 'cc-0') {
-
-        document.querySelector('#waive-your-copyright').classList.toggle('disable');
-    } else {
-
-        document.querySelector('#waive-your-copyright').classList.add('disable');
-    }
-
-});
-
-const attributionElement = document.querySelector("#require-attribution");
-
-const attributionChoice = form.elements["attribution"];
-
-attributionElement.addEventListener("change", (event) => {
-    console.log(attributionChoice.value);
-
-    document.querySelector('#allow-commercial-use').classList.toggle('disable');
-
-    document.querySelector('#allow-derivatives').classList.toggle('disable');
-
-    document.querySelector('#share-alike').classList.toggle('disable');
-
-    document.querySelector('#waive-your-copyright').classList.remove('disable');
-
-    document.querySelector('#confirmation').classList.toggle('disable');
+    });
 
 });
