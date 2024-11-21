9a0891fc17174f99849f25f322d02e13d3fe47e6
possumbilities <109087089+possumbilities@users.noreply.github.com>
2024-11-21 19:35:54 +0000

    add first round checks for statepaths

--- src/scripts.js
+++ src/scripts.js
@@ -8,12 +8,12 @@ let rawStatePathRoutes = [
     'do-you-know-which-license-you-need/yes/which-license-do-you-need/cc-by-nc-sa/attribution-details&license=cc-by-nc-sa',
     'do-you-know-which-license-you-need/yes/which-license-do-you-need/cc-by-nc-nd/attribution-details&license=cc-by-nc-nd',
     
-    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commerical-use/yes/allow-derivatives/yes/share-alike/yes/confirmation+ownership+read+revocation/attribution-details&license=cc-by',
-    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commerical-use/yes/allow-derivatives/yes/share-alike/no/confirmation+ownership+read+revocation/attribution-details&license=cc-by-sa',
-    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commerical-use/yes/allow-derivatives/no/confirmation+ownership+read+revocation/attribution-details&license=cc-by-nd',
-    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commerical-use/no/allow-derivatives/yes/share-alike/yes/confirmation+ownership+read+revocation/attribution-details&license=cc-by-nc',
-    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commerical-use/no/allow-derivatives/yes/share-alike/no/confirmation+ownership+read+revocation/attribution-details&license=cc-by-nc-sa',
-    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commerical-use/no/allow-derivatives/no/confirmation+ownership+read+revocation/attribution-details&license=cc-by-nc-nd',
+    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commercial-use/yes/allow-derivatives/yes/share-alike/no/confirmation+ownership+read+revocation/attribution-details&license=cc-by',
+    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commercial-use/yes/allow-derivatives/yes/share-alike/yes/confirmation+ownership+read+revocation/attribution-details&license=cc-by-sa',
+    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commercial-use/yes/allow-derivatives/no/confirmation+ownership+read+revocation/attribution-details&license=cc-by-nd',
+    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commercial-use/no/allow-derivatives/yes/share-alike/yes/confirmation+ownership+read+revocation/attribution-details&license=cc-by-nc',
+    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commercial-use/no/allow-derivatives/yes/share-alike/no/confirmation+ownership+read+revocation/attribution-details&license=cc-by-nc-sa',
+    'do-you-know-which-license-you-need/no/require-attribution/yes/allow-commercial-use/no/allow-derivatives/no/confirmation+ownership+read+revocation/attribution-details&license=cc-by-nc-nd',
     'do-you-know-which-license-you-need/no/require-attribution/no/waive-your-copyright+waive+read/attribution-details&license=cc-0'
 ];
 
@@ -25,6 +25,8 @@ state.parts = [];
 state.parts[0] = 'do-you-know-which-license-you-need/yes/';
 state.parts[1] = 'which-license-do-you-need/cc-by';
 console.log(state.parts);
+
+//state.possibilities = [];
 
 state.current = state.parts.join('');
 console.log(state.current);
@@ -45,7 +47,7 @@ rawStatePathRoutes.forEach((path, index) => {
     state.possibilities[license].push(statePath[0]);
     
  });
- console.log(state.possibilities);
+ //console.log(state.possibilities);
 
 const fieldsets = document.querySelectorAll('fieldset'); 
 // [T]: filter this down to ones not currently set to disable?
@@ -81,8 +83,47 @@ fieldsets.forEach((element, index) => {
        
         //console.log(element.id + '/' + event.target.value + '/' );
 
-        state.parts[index] = element.id + '/' + event.target.value + '/';
+        state.parts[index] = element.id + '/' + event.target.value;
 
+
+        // check if checkbox, with siblings
+        if (event.target.getAttribute('type') == 'checkbox') {
+            //state.parts[index] = element.id + '+' + event.target.value;
+            
+            let checkboxElements = element.querySelectorAll('input[type="checkbox"]');
+            let checkboxes = [];
+
+            checkboxElements.forEach((checkbox, index) => { 
+                if (checkbox.checked) {
+                    checkboxes[index] = checkbox.value;
+                    //console.log(checkbox.value);
+                }
+                
+            });
+
+            //console.log(checkboxes);
+
+            let joinedCheckboxes = checkboxes.filter(Boolean).join('+');
+
+            state.parts[index] = element.id + '+' + joinedCheckboxes;
+        }
+
+        if (event.target.getAttribute('type') == 'text') {
+
+            state.parts[index] = element.id;
+
+        }
+
+        // borked!
+        if (index + 1 != state.parts.length - 1) {
+
+            //state.parts[index] = state.parts[index] + '/';
+            //console.log(index);
+            //console.log(state.parts.length - 1);
+
+        }
+        
+        
       
         // [T]: refactor three below as element.id instead?
 
@@ -115,16 +156,23 @@ fieldsets.forEach((element, index) => {
         state.props.license = 'unknown';
 
         // check and match possibilities
-        state.possibilities.forEach ((possibility, index) => { 
-            if(possibility.includes(state.current)) {
-                //state.props.license = state.possibilities[index];
-                console.log('at:'+ state.possibilities[index]);
+        Object.keys(state.possibilities).forEach((possibility, index) => {
+            if(state.possibilities[possibility].includes(state.current)) {
+                state.props.license = possibility;
+                console.log('matched');
             }
+            // console.log(possibility);
+            // console.log(state.possibilities[possibility]);
+            // console.log(state.current);
+
+            //console.log('possibility: ' + possibility);
+            //console.log('possibilities');
         });
 
         console.log(state.parts);
         console.log(state.current);
         console.log(state.props.license);
+        //console.log(state.possibilities);
 
         // if (state.current == 'do-you-know-which-license-you-need/yes/which-license-do-you-need/cc-0/') {
         //     let chosenLicense = 'cc-0';
