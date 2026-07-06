8cc60b98e544cf9b50be93d4e2b5f4d149673cb9
possumbilities <109087089+possumbilities@users.noreply.github.com>
2026-07-06 18:58:00 +0000

    update vocabulary core css and js

--- src/vocabulary/js/vocabulary.js
+++ src/vocabulary/js/vocabulary.js
@@ -1,32 +1,33 @@
-const exploreButton = document.querySelector('button.explore');
-const explorePanel = document.querySelector('.explore-panel');
-
-// explorePanel.classList.add('hide');
-if(exploreButton!==null && explorePanel!==null) {
-    exploreButton.addEventListener('click', (event) => {
-        explorePanel.classList.toggle('expand');
-        // explorePanel.classList.toggle('hide');
-    });
-}
-
 const menuButton = document.querySelector('button.expand-menu');
 const menuPanel = document.querySelector('.primary-menu');
 
 if(menuButton !== null && menuPanel !== null) {
     menuButton.addEventListener('click', (event) => {
         menuPanel.classList.toggle('expand');
-        // explorePanel.classList.toggle('hide');
     });
 }
 
-const attributionButton = document.querySelector('button.expand-attribution');
-const attributionPanel = document.querySelector('.attribution-panel');
+const menusWithChildren = document.querySelectorAll('main > aside.sidebar nav > ul > li:has(ul) > a');
 
-if (attributionButton !== null && attributionPanel !== null ) {
-    attributionButton.addEventListener('click', (event) => {
-        attributionButton.classList.toggle('selected');
-        attributionPanel.classList.toggle('expand');
-        // explorePanel.classList.toggle('hide');
+menusWithChildren.forEach((menu) => {
+    const expandButton = document.createElement('button');
+    expandButton.classList.add('expand');
+    expandButton.classList.add('icon-replace');
+    expandButton.classList.add('fa-angle-down');
+    expandButton.textContent = 'Expand';
+
+    menu.insertAdjacentElement("afterend", expandButton);
+});
+
+
+const collapsedButtons = document.querySelectorAll('button.expand');
+
+collapsedButtons.forEach((expander) => {
+
+    expander.parentElement.querySelector('ul').classList.toggle('hide');
+    
+    expander.addEventListener('click', (event) => {
+        event.target.parentElement.querySelector('ul').classList.toggle('hide');
     });
 
-}
+});
